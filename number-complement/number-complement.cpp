04715692c9eaab4ffe5321ed21f5c509b4c02067
Gaurav Kandpal <43899127+gaurav12gk@users.noreply.github.com>
2021-12-28 00:54:51 +0700

    Time: 0 ms (100.00%), Space: 6 MB (40.00%) - LeetHub

---
+++ number-complement/number-complement.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int findComplement(int n) {
+     unsigned mask = ~0; 
+        while(mask & n) 
+            mask = mask << 1;
+        return mask ^ (~n);
+    }
+};
