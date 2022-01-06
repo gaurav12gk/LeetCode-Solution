6f0d971f47efaf43b2f9856433a054b467845c6b
Gaurav Kandpal <43899127+gaurav12gk@users.noreply.github.com>
2022-01-06 11:22:53 +0700

    Time: 21 ms (24.45%), Space: 10.3 MB (48.52%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
         vector<int>cap(1001);
+        int maxe = 0 ;
         for(auto c: trips)
         {
             cap[c[1]]-=c[0];
             cap[c[2]]+=c[0];
+            maxe = max(maxe , c[2]);
         }
-    for(int i = 0 ;i<=1000;i++)
+    for(int i = 0 ;i<=maxe;i++)
     {
         capacity+=cap[i];
         if(capacity<0)return false;
