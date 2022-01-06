8636ac4ef9200c6f00c97385041210536899b3d7
Gaurav Kandpal <43899127+gaurav12gk@users.noreply.github.com>
2022-01-06 11:14:43 +0700

    Time: 99 ms (5.10%), Space: 22.3 MB (5.17%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -7,38 +7,37 @@ public:
     }
     bool carPooling(vector<vector<int>>& trip, int c) {
       
-             map<int,list<int>> in,out;
+             map<int,priority_queue<int>> in,out;
             sort(trip.begin(),trip.end(),cmp);
            
              for(auto x: trip){
                
                
-                in[x[1]].push_back(x[0]);
-                out[x[2]].push_back(x[0]);
+                in[x[1]].push(x[0]);
+                out[x[2]].push(x[0]);
             
             }
-            for(int i = 0;i<=1000;i++)
+             for(int i = 0;i<=1000;i++)
             {
-                if(out[i].size()!=0)
-                {
+            
+            
+               
                     while (out[i].size()!=0)
                   {
-                     c+=*out[i].begin();
-                     out[i].pop_front();
+                     c+=out[i].top();
+                     out[i].pop();
                   }
-                }
-                if(in[i].size()!=0)
-                {
+               
+              
                   while (in[i].size()!=0)
                   {
-                     c-=*in[i].begin();
-                     in[i].pop_front();
+                     c-=in[i].top();
+                     in[i].pop();
                   }
-                  
-                }
+           
                 if(c<0)return false;
             }
-        return true;
+       return true;
         
     }
 };
