089776ab5a3407e670902b5b68f7898ecf0fa8ab
Gaurav Kandpal <43899127+gaurav12gk@users.noreply.github.com>
2022-01-06 11:15:51 +0700

    Time: 48 ms (7.06%), Space: 22.3 MB (5.17%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
-   static bool cmp(vector<int> &a,vector<int> &b)
-    {
-         if(a[1]==b[1])return a[2] <b[2];
-            return a[1]<b[1];
-    }
+ 
     bool carPooling(vector<vector<int>>& trip, int c) {
       
              map<int,priority_queue<int>> in,out;
-            sort(trip.begin(),trip.end(),cmp);
-           
+            
              for(auto x: trip){
                
                
