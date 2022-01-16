337b02a82a8576e6fc6971c0cc18c0f0a2dcb587
Gaurav Kandpal <43899127+gaurav12gk@users.noreply.github.com>
2022-01-16 10:01:42 +0700

    Time: 26 ms (15.11%), Space: 17.8 MB (13.45%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
@@ -4,9 +4,9 @@ public:
         vector<int> pre(seats.size(),-1);
         vector<int> suff(seats.size(),-1);
             int lastOneleft = -1;
-        if(seats[0]==1)lastOneleft = 0;
+
         
-        for(int i = 1 ;i<seats.size();i++)
+        for(int i = 0 ;i<seats.size();i++)
         {
         if(seats[i]==1){
             lastOneleft = i;
@@ -15,8 +15,8 @@ public:
         }
         int n = seats.size();
         int lastOneright = -1;
-        if(seats[n-1]==1)lastOneright = n-1;
-        for(int i = n-2;i>=0;i--)
+     
+        for(int i = n-1;i>=0;i--)
         {
             if(seats[i]==1)
             {
@@ -24,10 +24,7 @@ public:
             }
             else suff[i] = lastOneright;
         }
-        for(auto c  :pre)cout<<c<<" ";
-        cout<<endl;
-        for(auto c: suff) cout<<c<<" ";
-        
+  
         int maxDist = 0 ; 
         for(int i = 0 ;i<n;i++)
         {
