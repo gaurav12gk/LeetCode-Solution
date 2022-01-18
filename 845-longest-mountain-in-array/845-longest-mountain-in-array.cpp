c429fcb6f6066d33fe4b2b65e59d653d203aec23
Gaurav Kandpal <43899127+gaurav12gk@users.noreply.github.com>
2022-01-19 00:12:55 +0700

    Time: 20 ms (74.53%), Space: 18.5 MB (83.71%) - LeetHub

--- 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
+++ 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
@@ -1,9 +1,10 @@
+
 class Solution
 {
 public:
+    
     int longestMountain(vector<int> &v)
     {
-        ios_base::sync_with_stdio(false) ; cin.tie(0) ; cout.tie(0);
         int ans = 0;
         int i = 0, j = 1;
         while (j < v.size())
@@ -18,15 +19,13 @@ public:
 
                 while (j < v.size() and v[j] < v[j - 1])
                 {
-
-                    ans = max(ans, j - i + 1);
+                     ans = max(ans, j - i + 1);     
                     j++;
                 }
-
-                j--;
+                 j--;
+                   
             }
-            i = j;
-            j++;
+           i = j++;
         }
         return ans;
     }
