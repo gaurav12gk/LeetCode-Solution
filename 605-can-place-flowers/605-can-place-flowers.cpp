f4c132f7f0d028fe31db757e5400924f3db1c293
Gaurav Kandpal <43899127+gaurav12gk@users.noreply.github.com>
2022-01-18 11:10:10 +0700

    Time: 49 ms (5.42%), Space: 20.3 MB (69.55%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.cpp
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -1,37 +1,44 @@
-class Solution {
+class Solution
+{
 public:
-    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int ans = 0 ;
-        if(n==0)return true;
-        if(flowerbed.size()==1)
+    bool canPlaceFlowers(vector<int> &flowerbed, int n)
+    {
+        int ans = 0;
+        if (n == 0)
+            return true;
+        if (flowerbed.size() == 1)
         {
-          
-            if(n==1 and flowerbed[0]==0)return true; 
+
+            if ( flowerbed[0] == 0)
+                return true;
             return false;
         }
-        for(int i = 0 ;i<flowerbed.size();i++)
+        for (int i = 0; i < flowerbed.size(); i++)
         {
-            if(i==0)
+            if (i == 0)
             {
-                if(flowerbed[i]==0 and flowerbed[i+1]==0)
+                if (flowerbed[i] == 0 and flowerbed[i + 1] == 0)
                 {
-                    ans++,flowerbed[i] = 1;
+                    ans++, flowerbed[i] = 1;
                 }
             }
-        else    if(i==flowerbed.size()-1)
+            else if (i == flowerbed.size() - 1)
             {
-                if(flowerbed[i]==0 and flowerbed[i-1]==0)
-                    ans++,flowerbed[i] = 1;
+                if (flowerbed[i] == 0 and flowerbed[i - 1] == 0)
+                    ans++, flowerbed[i] = 1;
             }
-          else  if(flowerbed[i]==0)
+            else if (flowerbed[i] == 0)
             {
-                if(flowerbed[i-1]==0 and flowerbed[i+1]==0){ans++;
-                flowerbed[i] = 1;}
-          
+                if (flowerbed[i - 1] == 0 and flowerbed[i + 1] == 0)
+                {
+                    ans++;
+                    flowerbed[i] = 1;
+                }
             }
         }
-        
-        if(ans >=n)return true; 
+
+        if (ans >= n)
+            return true;
         return false;
     }
 };
