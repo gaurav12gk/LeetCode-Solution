1c17bd32fd5f58e0459194e609660071af55f465
Gaurav Kandpal <43899127+gaurav12gk@users.noreply.github.com>
2022-01-19 00:40:47 +0700

    Time: 28 ms (15.72%), Space: 8 MB (87.19%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -1,35 +1,15 @@
 class Solution {
 public:
-    static   bool cmp(pair<char,int> &a,pair<char,int> &b)
-       {
-           if(a.second == b.second) return a.first > b.first;
-           return a.second > b.second;
-       }
     string frequencySort(string s) {
-            vector<pair<char,int>> v(123);
-            for(int i = 0 ;i<123;i++)
-            v[i].second = 0 ; 
-
-            for(auto c: s)
-            {
-                if(isupper(c))
-                {
-                    v[c].second++;
-                    v[c].first = c;
-                }
-                else {v[c].second++;v[c].first = c;}
-            }
-
-            sort(v.begin(),v.end(),cmp);
-            string ss; 
-            for(int i = 0 ;i<123;i++)
-            {
-                if(v[i].second ==0)break;
-                while(v[i].second--)
-                {
-                    ss+=v[i].first;
-                }
-            }
-           return ss;
+        int cnt[256] ={0};
+        
+        for(auto c: s)
+        cnt[c]++;
+        
+        sort(s.begin(),s.end(),[&](char a, char b){
+            if(cnt[a]==cnt[b])return a>b;
+            return cnt[a] > cnt[b];
+        });
+        return s;
     }
 };
