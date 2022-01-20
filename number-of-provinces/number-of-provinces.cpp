f88c294b66560bc55923f52ff248b39475281d03
Gaurav Kandpal <43899127+gaurav12gk@users.noreply.github.com>
2022-01-21 00:32:05 +0700

    Time: 31 ms, Memory: 13.8 MB - LeetHub

---
+++ number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+  vector<int> parent;
+    vector<int> rank;
+    int Find(int a)
+    {
+       if(parent[a]==-1)return a;
+        return parent[a] = Find(parent[a]);
+    }
+    void Union(int a, int b)
+    {
+        a = Find(a);
+        b = Find(b); 
+       if(rank[a] < rank[b])
+           swap(a,b);
+        rank[a]+=rank[b];
+        parent[b] = a;
+        
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+       parent.resize(isConnected.size()+1,-1);
+        rank.resize(isConnected.size()+1,1);
+        parent[0] = 1;
+        for(int i = 0 ;i<isConnected.size();i++)
+        {
+            for(int j = 0 ;j<isConnected[i].size();j++)
+            {
+                if(isConnected[i][j])
+                {
+                    if(Find(i+1)!=Find(j+1))
+                    {
+                            Union(i+1,j+1);
+                    }
+                }
+            }
+        }
+      int ans = 0 ;  
+        for(auto c: parent)
+        {
+            if(c==-1)ans++;
+        }
+     return ans;}
+   
+};
