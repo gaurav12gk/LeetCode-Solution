01eb5871177f5249035a22341fc3e26663a44a80
Gaurav Kandpal <43899127+gaurav12gk@users.noreply.github.com>
2022-02-02 11:38:02 +0700

    Time: 12 ms (87.76%), Space: 8.7 MB (42.26%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     vector<int> findAnagrams(string txt, string pat) {
         vector<int> res;
-          map<char,int> pathash,txthas; 
-  for(auto c: pat)pathash[c]++; 
+          vector<int> txthas(26),pathash(26);
+  for(auto c: pat)pathash[c-'a']++; 
   int i = 0 , j = 0 ; 
 
   while(j<txt.size())
   {
-	  txthas[txt[j]]++; 
+	  txthas[txt[j]-'a']++; 
 	 if(j-i+1==pat.size())
 	 { 
 		 if(txthas == pathash)
@@ -16,8 +16,8 @@ public:
 			 	
 				 res.push_back(i);
 		 }
-		 txthas[txt[i]]--; 
-		 if(txthas[txt[i]]==0)txthas.erase(txt[i]); 
+		 txthas[txt[i]-'a']--; 
+		
 		 i++;
 		 j++; 
 	 }else j++;
