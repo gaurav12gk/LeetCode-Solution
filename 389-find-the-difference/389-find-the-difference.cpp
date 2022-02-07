47fadc6766b524dd91a101688bd4c5472ecf9187
Gaurav Kandpal <43899127+gaurav12gk@users.noreply.github.com>
2022-02-07 16:48:03 +0700

    Time: 8 ms (17.39%), Space: 6.8 MB (25.35%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
      char y = 0 ;
-        for(auto c: s)
-            y^=c;
-        for(auto c:t)y^=c;
-        return y;
+        for(auto c: s+t)
+            y^=c; return y;
     }
 };
