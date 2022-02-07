c270a8e25d1a459dddd5c4c094f78baf100e5ed1
Gaurav Kandpal <43899127+gaurav12gk@users.noreply.github.com>
2022-02-07 10:54:04 +0700

    Time: 13 ms (31.19%), Space: 10.7 MB (65.83%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.cpp
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -1,26 +1,26 @@
-class Solution {
+class Solution
+{
 public:
     vector<string> res;
-    void helper(string s,int i)
-       {
-           if(i==s.size()){
-               res.push_back(s);
-               return;
-           }
-       
-        if(isdigit(s[i])){
-            helper(s,i+1);
+    void helper(string s, int i)
+    {
+        if (i == s.size())
+        {
+            res.push_back(s);
+            return;
         }
-        else{
-            helper(s,i+1);
-            if(isupper(s[i]))s[i] = tolower(s[i]);
-           else if(islower(s[i]))s[i] = toupper(s[i]);
-         
-            helper(s,i+1);
+
+     helper(s,i+1);
+       if(!isdigit(s[i]))
+        {
+            s[i]^= 1<<5;
+
+            helper(s, i + 1);
         }
-       }
-    vector<string> letterCasePermutation(string s) {
-   helper(s,0);
+    }
+    vector<string> letterCasePermutation(string s)
+    {
+        helper(s, 0);
         return res;
     }
 };
