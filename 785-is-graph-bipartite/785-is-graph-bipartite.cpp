c5b3cfa44a891f7d06ec0436078007aa3415e756
Gaurav Kandpal <43899127+gaurav12gk@users.noreply.github.com>
2022-02-12 00:50:16 +0700

    Time: 81 ms (6.06%), Space: 16.1 MB (9.95%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -18,12 +18,37 @@ public:
        }
        return true;
    }
+    bool bfs(int source,int c, vector<vector<int>> v)
+    {
+        queue<int> q;
+        q.push(source);
+        visited[source] = true;
+        while(q.size())
+        {
+            auto cur = q.front();
+            q.pop();
+            visited[cur] = true;
+            for(auto child: v[cur])
+            {
+                if(!visited[child])
+                    
+                {
+                  q.push(child);
+                    color[child] = color[cur]^1;
+                }
+                if(color[child] == color[cur])
+                    return false;
+            }
+            
+        }
+        return true;
+    }
     bool isBipartite(vector<vector<int>>& graph) {
         visited.resize(graph.size()+1);
         color.resize(graph.size()+1);
       for(int i = 0 ;i<graph.size();i++)
           if(!visited[i])
-              if(!dfs(i,0,graph))return false;
+              if(!bfs(i,0,graph))return false;
         
         return true;
        
