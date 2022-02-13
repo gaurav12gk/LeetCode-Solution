723a7469f2769297ff11ea30ab4e4980c7d86cdd
Gaurav Kandpal <43899127+gaurav12gk@users.noreply.github.com>
2022-02-13 17:38:19 +0700

    Time: 0 ms (100.00%), Space: 7.3 MB (47.20%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> res; 
-    void helper(vector<int> v, int idx, vector<int> path)
-    {
-        if(idx>=v.size())
-        {
-            res.push_back(path);
-            return;
-        }
-       
-        path.push_back(v[idx]);
-        helper(v,idx+1,path);
-        path.pop_back();
-         helper(v,idx+1,path);
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-     vector<int> path;
-        helper(nums,0,path);
+     vector<vector<int>> res; 
+        for(int i = 0 ;i<pow(2,nums.size());i++)
+        {
+            vector<int> v;
+            for(int j = 0 ;j<31;j++)
+            {
+                int k = 1 << j; 
+                if(k&i)
+                {
+                    v.push_back(nums[j]);
+                }
+            }
+            res.push_back(v);
+        }
         return res;
     }
 };
