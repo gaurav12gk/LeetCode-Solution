3eee642fc9f4c1fb98f3261629a4499393d12113
Gaurav Kandpal <43899127+gaurav12gk@users.noreply.github.com>
2022-02-16 20:29:53 +0700

    Time: 4 ms (92.87%), Space: 9.2 MB (6.29%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,43 +1,39 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>res(nums2.size());
-        stack<int> s;
-        
-    for(int i =nums2.size()-1;i>=0;i--)
-    {
-        if(s.size()==0)
-        {
-            res[i] = -1;
-            s.push(nums2[i]);
-            continue;
-        }
-        while(s.top()<nums2[i])
-         { 
-            s.pop();
-        if(s.size()==0)break;
-        }
-        if(s.size()==0){
-            res[i]=-1;
-            s.push(nums2[i]);
-            continue;
-        }
-        res[i] = s.top();
-        s.push(nums2[i]);
-        
-    }
-        // for(auto c: res)
-        //     cout<<c<<" ";
-        vector<int> ans;
-    //    return ans;
         map<int,int> m; 
-        for(int i = 0 ;i<nums2.size();i++)
+        stack<int> s; 
+        int n = nums2.size();
+        m[nums2[n-1]] = -1;
+        s.push(nums2[n-1]);
+        for(int i = n-2;i>=0;i--)
         {
-            m[nums2[i]] = res[i];
+            if(s.size()==0)
+            {
+                m[nums2[i]]=-1;
+                s.push(nums2[i]);
+                continue;
+            }
+            while(s.size() and s.top()<nums2[i])
+            {
+                s.pop();
+            }
+            if(s.size()==0)
+            {
+                m[nums2[i]] = -1;
+                s.push(nums2[i]);
+                continue;
+            }
+            else {
+                m[nums2[i]] = s.top();
+                s.push(nums2[i]);
+            }
         }
-       
-        for(auto c :nums1)
-            ans.push_back(m[c]);
-        return ans;
+        vector<int> res; 
+        for(auto c: nums1)
+        {
+            res.push_back(m[c]);
+        }
+        return res;
     }
 };
