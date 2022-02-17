232face14b5e2323fe1b31596644e783ee08df47
Gaurav Kandpal <43899127+gaurav12gk@users.noreply.github.com>
2022-02-17 08:20:03 +0700

    Time: 274 ms (9.30%), Space: 95 MB (6.09%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> res;
-    void comb(vector<int> v, int target,vector<int> path,int n, int idx)
+    vector<vector<int>> res; 
+    void helper(int idx, int target, vector<int> path, vector<int> v)
     {
-        if(idx == n)
+        if(idx == v.size())
         {
-            if(target == 0)
-                res.push_back(path);
+            if(target==0)res.push_back(path);
             return;
         }
         if(v[idx]<=target)
         {
             path.push_back(v[idx]);
-            comb(v,target-path.back(),path,n,idx);
+            helper(idx,target-path.back(),path,v);
             path.pop_back();
         }
-        comb(v,target,path,n,idx+1);
+        helper(idx+1,target,path,v);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-       vector<int> path;
-        comb(candidates,target,path,candidates.size(),0);
+        vector<int>path;
+        helper(0,target,path,candidates);
         return res;
     }
 };
