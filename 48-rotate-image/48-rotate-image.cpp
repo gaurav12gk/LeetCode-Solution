15d5afdbca76f3583e2336800b22f683e8302d91
Gaurav Kandpal <43899127+gaurav12gk@users.noreply.github.com>
2022-02-17 19:33:13 +0700

    Time: 4 ms (48.89%), Space: 7.2 MB (33.85%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -5,7 +5,7 @@ public:
         int n = matrix.size();
         for(int i = 0 ;i<n;i++)
         {
-            for(int j = 0;j<i;j++)
+            for(int j = i;j<n;j++)
                 swap(matrix[i][j],matrix[j][i]);
         }
     for(int i= 0 ;i<n;i++)
