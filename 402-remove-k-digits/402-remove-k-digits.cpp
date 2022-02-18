2b59342908ba3d8e66e10a7d3cc8146cd80b0c7d
Gaurav Kandpal <43899127+gaurav12gk@users.noreply.github.com>
2022-02-19 01:22:50 +0700

    Time: 3 ms (91.01%), Space: 7.1 MB (35.82%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     string removeKdigits(string nums, int k) {
    if(nums.size()<=k or k ==0)return "0";
-        stack<char> s; 
+    vector<char> s; 
         string res="";
-        s.push(nums[0]);
+        s.push_back(nums[0]);
         for(int i = 1;i<nums.size();i++)
         {
-            while(k and s.size() and s.top()>nums[i])
-                s.pop() , k--;
+            while(k and s.size() and s.back()>nums[i])
+                s.pop_back() , k--;
             
-            s.push(nums[i]);
+            s.push_back(nums[i]);
             
             // if(s.size()==1 and s.top()=='0')
             //     s.pop();
         }
         while(k)
-            s.pop(),k--;
+            s.pop_back(),k--;
         while(s.size())
-            res.push_back(s.top()),s.pop();
+            res.push_back(s.back()),s.pop_back();
         
         reverse(res.begin(),res.end());
        
-        cout<<res;
+      //  cout<<res;
         int i = 0 ; 
         while(res[i]=='0')
             i++;
