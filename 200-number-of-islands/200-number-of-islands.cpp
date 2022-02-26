fcd388a1dc7463591719bdcb94643b1ca3f94471
Gaurav Kandpal <43899127+gaurav12gk@users.noreply.github.com>
2022-02-26 20:21:11 +0700

    Time: 138 ms (7.54%), Space: 12.5 MB (43.53%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>> &grid,int x ,int y)
+    vector<vector<bool>> visit;
+    void dfs(int x , int y , vector<vector<char>> &grid)
     {
-    if(x>grid.size()-1 or y>grid[0].size()-1 or x<0 or y<0 or grid[x][y]=='0')return;
-    
-        grid[x][y] = '0';
-        dfs(grid,x-1,y);
-        dfs(grid,x+1,y);
-        dfs(grid,x,y+1);
-        dfs(grid,x,y-1);
+        cout<<"Hello";
+        if(x<0 or y<0 or x>grid.size()-1 or y > grid[0].size()-1 or grid[x][y] == '0')return ;
+        grid[x][y] ='0';
+        dfs(x-1,y,grid);
+        dfs(x,y-1,grid);
+        dfs(x+1,y,grid);
+        dfs(x,y+1,grid);
     }
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m =grid[0].size();
-        int cnt = 0 ; 
-        for(int i = 0;i<n;i++)
-            for(int j = 0 ;j<m;j++)
+        //DFS implementation 
+        visit.resize(grid.size(),vector<bool>(grid[0].size()));
+        
+        int cnt = 0;
+        for(int i = 0 ;i<grid.size();i++)
+            for(int j = 0 ;j<grid[0].size();j++)
             {
-            if(grid[i][j]=='1')
-            {
-                dfs(grid,i,j);
-                cnt++;
-            }
+                if( grid[i][j] == '1')
+                  {  
+                    dfs(i,j,grid);
+                    cnt++;
+                }
             }
         return cnt;
     }
