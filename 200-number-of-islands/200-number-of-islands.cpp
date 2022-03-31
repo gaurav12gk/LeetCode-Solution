651c22c37732266f035c4c55867e22ee750d0124
Gaurav Kandpal <43899127+gaurav12gk@users.noreply.github.com>
2022-03-31 21:45:00 +0700

    Time: 93 ms (10.67%), Space: 18.2 MB (17.44%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,29 +1,43 @@
 class Solution {
 public:
-    vector<vector<bool>> visit;
-    void dfs(int x , int y , vector<vector<char>> &grid)
+    int dx[4] = {0,1,0,-1};
+    int dy[4] = {1,0,-1,0};
+    bool isValid(int x, int y , int n , int m)
     {
-        
-        if(x<0 or y<0 or x>grid.size()-1 or y > grid[0].size()-1 or grid[x][y] == '0')return ;
-        grid[x][y] ='0';
-        dfs(x-1,y,grid);
-        dfs(x,y-1,grid);
-        dfs(x+1,y,grid);
-        dfs(x,y+1,grid);
+        if(x<0 or y<0 or x>n-1 or y> m-1)return false; 
+        return true;
+    }
+    void bfs(int x , int y , vector<vector<char>> &grid)
+    {
+       // cout<<"Helo";
+        grid[x][y] = '0';
+        queue<pair<int,int>> q;
+        q.push({x,y});
+        while(q.size())
+        {
+            auto cur = q.front(); 
+            q.pop();
+          
+            for(int i =  0 ;i<4;i++)
+            {
+                int newx = cur.first+dx[i];
+                int newy = cur.second + dy[i];
+                if(isValid(newx,newy,grid.size(),grid[0].size()) and grid[newx][newy]=='1')
+                    q.push({newx,newy}),  grid[newx][newy] = '0';
+                
+            }
+        }
+       
     }
     int numIslands(vector<vector<char>>& grid) {
         //DFS implementation 
-        visit.resize(grid.size(),vector<bool>(grid[0].size()));
-        
+       
         int cnt = 0;
         for(int i = 0 ;i<grid.size();i++)
             for(int j = 0 ;j<grid[0].size();j++)
             {
                 if( grid[i][j] == '1')
-                  {  
-                    dfs(i,j,grid);
-                    cnt++;
-                }
+                  {  bfs(i,j,grid),cnt++;}
             }
         return cnt;
     }
